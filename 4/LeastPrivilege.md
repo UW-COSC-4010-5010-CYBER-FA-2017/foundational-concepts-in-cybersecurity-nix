f956e6e48eab7f9da2ff946dd2d83c36dad4dc57
Nikhil Shetty <nikhil.j.shetty@gmail.com>
2017-11-01 04:02:19 -0600

    Lease priviliges

--- 4/LeastPrivilege.md
+++ 4/LeastPrivilege.md
@@ -1,14 +1,48 @@
 ## Exempler:
-
+This example takes care that the MALWARE app which required Administrator
+priviliges is not allowed to install as a user.
 ```
-
+# ./4Exemplar
+Welcome we have two logins accounts
+ 1.User
+ 2.Admin
+ 3.Quit
+Please choose option :1
+Please select task
+ 1.Install Package
+ 2.Exit
+Please choose option :1
+You can install the following packages
+ 1.BROWSER
+ 2.EDITOR
+ 3.MALWARE
+ 4.Exit
+Please choose option :3
+..............UNAUTHORISED
 ```
 
 
 ## NonExample:
-
+In this case even a user can install a Malware cause an ordinary user is also
+granted Admin priviliges.
 
 ```
-
+# ./4NonExample
+Welcome we have two logins accounts
+ 1.User
+ 2.Admin
+ 3.Quit
+Please choose option :1
+Please select task
+ 1.Install Package
+ 2.Exit
+Please choose option :1
+You can install the following packages
+ 1.BROWSER
+ 2.EDITOR
+ 3.MALWARE
+ 4.Exit
+Please choose option :3
+................Installing Malware
 ```
 
---
+++ 4/src/4NonExample.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+using namespace std;
+
+int user;
+int login() {
+    int user;
+    cout <<"Welcome we have two logins accounts" <<endl
+         <<" 1.User" <<endl
+         <<" 2.Admin" <<endl
+         <<" 3.Quit" << endl
+         <<"Please choose option :";
+    cin >> user;
+    return user;
+}
+
+int installOrExit() {
+    int choice;
+    cout <<"Please select task" << endl
+         <<" 1.Install Package" << endl
+         <<" 2.Exit" << endl
+         <<"Please choose option :";
+    cin >> choice;
+    return choice;
+}
+
+int package() {
+    int package;
+    cout << "You can install the following packages" << endl;
+    cout << " 1.BROWSER" << endl
+         << " 2.EDITOR" << endl
+         << " 3.MALWARE" << endl
+         << " 4.Exit" <<endl
+         << "Please choose option :";
+    cin >> package;
+    return package;
+}
+
+void packageMenu() {
+    while (1) {
+        switch( package() ) {
+        case 1:
+            cout << ".................Installing Browser" << endl;
+            break;
+        case 2:
+            cout << ".................Installing Editor" <<endl;
+            break;
+        case 3:
+            cout << "................Installing Malware" << endl;
+            break;
+        default:
+            return;
+        }
+    }
+}
+
+void installOrExitMenu() {
+    while ( 1 ) {
+        switch( installOrExit() ) {
+        case 1:
+            packageMenu();
+            break;
+        default:
+            return;
+        }
+    }
+}
+
+void loginMenu() {
+    while ( 1 ) {
+        switch( user=login() ) {
+        case 1:
+        case 2:
+            installOrExitMenu();
+            break;
+        default:
+            return;
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    loginMenu();
+    return 0;
+}
